ad026f81deb666ef81b22fe066e674f1eb40fe11
Kyryl <bodakvakyryl@gmail.com>
2026-06-23 14:09:43 +0200

    feat: nest server setup

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { json } from "express";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.use(json());
+  const port = process.env.PORT ?? 5000;
+  await app.listen(port);
+  console.log(`Server running at http://localhost:${port}`);
+}
+bootstrap();
